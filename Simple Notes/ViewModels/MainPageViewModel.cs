1bfe171bfd10bedc74a6a86ccda8abb2b52c32be
Vadim <defender834@gmail.com>
2023-01-25 13:06:16 +0700

    Base MVVM Infrastructure

---
+++ Simple Notes/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Simple_Notes.ViewModels.Base;
+
+namespace Simple_Notes.ViewModels
+{
+    internal class MainPageViewModel : ViewModel
+    {
+        #region Title : string - Page title
+
+        private string _title = "Проверка";
+
+        public string Title
+        {
+            get => _title;
+            set => SetField(ref _title, value);
+        }
+
+        #endregion
+
+    }
+}
--- Simple Notes/Views/MainPage.xaml.cs
+++ Simple Notes/Views/MainPage.xaml.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Core;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Notifications;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +15,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Simple_Notes.ViewModels;
 
 // Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
 
@@ -24,7 +28,14 @@ namespace Simple_Notes
     {
         public MainPage()
         {
+            //var coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+            //coreTitleBar.ExtendViewIntoTitleBar = true;
+
+            
             this.InitializeComponent();
+            var context = this.DataContext as MainPageViewModel;
+            if (context == null) return;
+            ApplicationView.GetForCurrentView().Title = context.Title;
         }
     }
 }
